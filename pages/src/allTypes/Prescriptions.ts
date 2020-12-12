a8508e7842ee02ec26a1a7fc0200b4f03214f62f
tomsa78 <tomsa78@hotmail.it>
2020-12-12 17:32:11 -0500

    added search data

--- pages/src/allTypes/Prescriptions.ts
+++ pages/src/allTypes/Prescriptions.ts
@@ -1,15 +1,16 @@
 import { objectType } from "@nexus/schema";
 import { Dosage } from "./Dosage";
 export const Prescriptions = objectType({
-    name: "Prescriptions",
+    name: "Prescriptions",    
     definition(t) {
         t.string('name');
+        t.string('generic_name');
         t.string('manufacturer');
         t.list.string('form');
         t.list.field('dosage', {
             type:Dosage,
-            resolve(dosage) {
-                return dosage;
+            resolve(dosage) {                
+                return dosage.dosage;                
             }
         });
         t.list.string('quantity');
--- pages/src/allTypes/Query.ts
+++ pages/src/allTypes/Query.ts
@@ -1,4 +1,4 @@
-import { queryType } from "@nexus/schema";
+import { stringArg, queryType } from "@nexus/schema";
 import { Prescriptions } from "./Prescriptions";
 import { data } from "../data";
 
@@ -7,6 +7,47 @@ export const Query = queryType({
          t.list.field("prescriptions",{
              type: Prescriptions,
              resolve:() => data.prescritions,
+         });
+
+         t.list.field("prescription",{
+             type:Prescriptions,             
+             args:{prescription: stringArg()},
+             resolve:(root,{prescription}:{prescription:string}, ctx)=>{
+                 let ar = [];
+                 let lenght_of_the_prescription_to_search = prescription.trim().length;
+                 let it_matches = true; 
+                data.prescritions.forEach((value) =>{
+                          
+                         let v = value.name.toLowerCase();
+                         it_matches = true;
+                        for(let i = 0 ; i < lenght_of_the_prescription_to_search; i++)
+                        {
+                            if(v[i] !== prescription.toLowerCase()[i])
+                                    it_matches = false;
+                        }     
+                         if(it_matches)
+                         {
+
+                           ar.push(value);
+                         }
+                         it_matches = true;
+                         let g = value.generic_name.toLowerCase();
+                         for(let i = 0 ; i < lenght_of_the_prescription_to_search; i++)
+                         {
+                            if(g[i] !== prescription.toLowerCase()[i])
+                                    it_matches = false;
+                         }
+                         if(it_matches)
+                         {
+                           ar.push(value);
+                         }
+                         
+
+                });                 
+             
+             return ar;
+            }
+            
          });
     }
 })
