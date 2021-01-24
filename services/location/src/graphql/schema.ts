b6c0f7ac5ee09985761286fc69b08b73666feb1b
tomsa78 <tomsa78@hotmail.it>
2021-01-23 19:21:45 -0500

    added nginx-ingress and load balancer

--- services/location/src/graphql/schema.ts
+++ services/location/src/graphql/schema.ts
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql(`       
         
+
         type GetLocationFromZipQuery{
              code:Int
              message:String
