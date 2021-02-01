b6521c7760055a71435342e75ca5245be471f2ef
tomsa78 <tomsa78@hotmail.it>
2021-02-01 01:45:34 -0500

    added log file

---
+++ services/location/src/helper/writeToLog.ts
@@ -0,0 +1,21 @@
+const { WriteToLogBaseClass } = require('./../../../../common/');
+const json_p  = require('./../../package.json');
+
+
+class writeToLog extends WriteToLogBaseClass {
+
+            constructor(fileName:string,serviceName:string){
+                super(fileName,serviceName)
+                
+            }
+            public writeToLog(value:string)
+            {
+                      super.writeToLog(value);
+            }
+            public getServiceName(){
+                   
+                return super.getServiceName();
+            }
+}
+
+export default new writeToLog('log',json_p.name);
