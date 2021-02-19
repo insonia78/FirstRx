e0c19401c13f8aaa2e94f1a3371097010601072c
tomsa78 <tomsa78@hotmail.it>
2021-02-18 22:54:06 -0500

    fixed errors

--- services/prescriptions/src/graphql/resolvers.ts
+++ services/prescriptions/src/graphql/resolvers.ts
@@ -1,26 +1,26 @@
 const request = require('postman-request');
 const crypto = require('crypto');
 import { response } from 'express';
-import  {writeToLog}  from './../../src/helper/writeToLog';
+import { writeToLog } from './../../src/helper/writeToLog';
 const soapRequest = require('easy-soap-request');
 const fs = require('fs');
 import path from 'path';
 
-let private_key:string;
- 
- fs.readFile(path.join(process.cwd(),"firstrx.key"),(err:any,data:any)=>{
-     private_key = data;   
- 
- });
- 
+let private_key: string;
+
+fs.readFile(path.join(process.cwd(), "firstrx.key"), (err: any, data: any) => {
+    private_key = data;
+
+});
+
 module.exports = {
     Mutation: {
-        prescription: async (parent: any, args: any, context: any, info: any) => {       
-            const signer = crypto.createSign('RSA-SHA256'); 
+        prescription: async (parent: any, args: any, context: any, info: any) => {
+            const signer = crypto.createSign('RSA-SHA256');
             signer.write(new Date().toISOString());
             signer.end();
             const signature = signer.sign(private_key, 'base64')
-            const xml =`
+            const xml = `
             <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
             <soapenv:Header>
                 <mkt:AuthenticationHeader>
@@ -34,22 +34,22 @@ module.exports = {
                   <listLatLonOut xsi:type="xsd:string">&lt;?xml version='1.0'?&gt;&lt;dwml version='1.0' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='https://graphical.weather.gov/xml/DWMLgen/schema/DWML.xsd'&gt;&lt;latLonList&gt;32.9612,-96.8372&lt;/latLonList&gt;&lt;/dwml&gt;</listLatLonOut>
                </ns1:LatLonListZipCodeResponse>
                 </SOAP-ENV:Body>
-            </SOAP-ENV:Envelope>`;   
-         return await new Promise((resolve, reject) => {
-                         try {
+            </SOAP-ENV:Envelope>`;
+            return await new Promise((resolve, reject) => {
+                try {
                     const value = args.value;
-                    let url = process.env.MEDIMPACT_URL;    
+                    let url = process.env.MEDIMPACT_URL;
                     let envVariables = process.env.APIKEYS;
                     let obj = JSON.parse(JSON.stringify(envVariables));
-                   const options ={
-                       url: url,
-                       headers:{
-                           authorization: obj['medimpact-token'],
-                           'CC-Timestamp-Signature':signature
-                       }
-                   }
-                    
-                    
+                    const options = {
+                        url: url,
+                        headers: {
+                            authorization: obj['medimpact-token'],
+                            'CC-Timestamp-Signature': signature
+                        }
+                    }
+
+
                     request(options, (error: any, response: any, body: any) => {
                         if (error) {
                             console.log(`${writeToLog.getServiceName()} = ${error}`);
@@ -59,28 +59,28 @@ module.exports = {
                         }
                         else {
 
-                            console.log('response from server',body); 
-                            // if (Object.keys(body = JSON.parse(body)).length !== 0 || body['predictions'].length > 0) {
-                            //     // console.log(body);
-                            //     // console.log('inside predictions',body['predictions']);
-                            //     // console.log('inside predictions1',body.predictions);
-                            //     // console.log('inside predictions1',body.predictions.length);
-                            //     if (body['predictions'].length === 1) {                                    
-                            //         getAddressByGeoLocation(body,body.predictions[0].place_id,resolve)    
-                            //     }
-                            //     else {
+                            console.log('response from server', body);
+                            if (Object.keys(body = JSON.parse(body)).length !== 0) {
+                                // console.log(body);
+                                // console.log('inside predictions',body['predictions']);
+                                // console.log('inside predictions1',body.predictions);
+                                // console.log('inside predictions1',body.predictions.length);
+                                // if (body['predictions'].length === 1) {
+                                //     getAddressByGeoLocation(body, body.predictions[0].place_id, resolve)
+                                // }
+                                // else {
 
-                            //         if (body.error !== undefined) {
-                            //             console.log(`${writeToLog.getServiceName()} = ${body.error}`);
-                            //             writeToLog.writeToLog(`code:400, error:InternalServerError, message:${body.error}`);
-                            //             Object.assign(body, { code: 400, message: body.error });
-                            //         }
-                            //         else {
-                            //             Object.assign(body, { code: 200 });
-                            //         }
-                            //         resolve(body);
-                            //     }
-                                
+                                //     if (body.error !== undefined) {
+                                //         console.log(`${writeToLog.getServiceName()} = ${body.error}`);
+                                //         writeToLog.writeToLog(`code:400, error:InternalServerError, message:${body.error}`);
+                                //         Object.assign(body, { code: 400, message: body.error });
+                                //     }
+                                //     else {
+                                //         Object.assign(body, { code: 200 });
+                                //     }
+                                //     resolve(body);
+                                // }
+
 
                             }
                             else {
@@ -92,16 +92,7 @@ module.exports = {
                         }
 
                     });
-                    }
-                    else{
-                        resolve({code:'404',error:'Not Found',message:'Value not found'});
-                    }
-
-
-
-
-                }
-                catch (e) {
+                }catch (e) {
                     // console.log(`${writeToLog.getServiceName()} =${e.message}`);
                     // writeToLog.writeToLog(`code:500, error:'Internal Server Error' messase:${e.message}`);
                     resolve({ code: '500', error: 'Internal Server Error', message: e.message });
