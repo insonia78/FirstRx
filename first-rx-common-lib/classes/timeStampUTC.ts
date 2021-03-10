bf515f75b72a4373dfe2b54fbe90cde1e5d0eb9f
tomsa78 <tomsa78@hotmail.it>
2021-03-10 07:21:28 -0500

    update

--- first-rx-common-lib/classes/timeStampUTC.ts
+++ first-rx-common-lib/classes/timeStampUTC.ts
@@ -27,7 +27,7 @@ class TimeStampUTC {
         else if (timezone_offset_min > 0)
             timezone_standard = '-' + offset_hrs + ':' + offset_min;
         else if (timezone_offset_min == 0)
-            timezone_standard = 'Z';
+            timezone_standard = '+0:00';
 
 
         return timezone_standard;
@@ -42,7 +42,7 @@ class TimeStampUTC {
             current_hrs = dt.getHours().toString(),
             current_mins = dt.getMinutes().toString(),
             current_secs = dt.getSeconds().toString(),
-            //current_millisecs = dt.getMilliseconds().toString(),
+            current_millisecs = dt.getMilliseconds().toString(),
             current_datetime;
 
         // Add 0 before date, month, hrs, mins or secs if they are less than 0
@@ -51,14 +51,14 @@ class TimeStampUTC {
         current_hrs = parseInt(current_hrs) < 10 ? '0' + current_hrs : current_hrs;
         current_mins = parseInt(current_mins) < 10 ? '0' + current_mins : current_mins;
         current_secs = parseInt(current_secs) < 10 ? '0' + current_secs : current_secs;
-        // if(current_millisecs.length < 3 && current_millisecs.length > 1)
-        //     current_millisecs = '0'+ current_millisecs;
-        // else if(current_millisecs.length  < 2)
-        //     current_millisecs = '00'+ current_millisecs;       
-        //current_millisecs = parseInt(current_millisecs) < 10 ? '00' + current_millisecs : current_millisecs;
+        if(current_millisecs.length < 3 && current_millisecs.length > 1)
+            current_millisecs = '0'+ current_millisecs;
+        else if(current_millisecs.length  < 2)
+            current_millisecs = '00'+ current_millisecs;       
+        current_millisecs = parseInt(current_millisecs) < 10 ? '00' + current_millisecs : current_millisecs;
 
         
-        current_datetime = current_year + '-' + current_month + '-' + current_date + 'T' + current_hrs + ':' + current_mins + ':' + current_secs;
+        current_datetime = current_year + '-' + current_month + '-' + current_date + 'T' + current_hrs + ':' + current_mins + ':' + current_secs +':'+current_millisecs;
 
         return current_datetime;
 
