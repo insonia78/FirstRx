432105fc4a9dec094a79b3da111fbde1c089e75a
tomsa78 <tomsa78@hotmail.it>
2021-03-09 20:14:43 -0500

    update

--- services/prescriptions/src/graphql/resolvers.ts
+++ services/prescriptions/src/graphql/resolvers.ts
@@ -62,7 +62,7 @@ module.exports = {
                         soapenv:encodingStyle="http://www.w3.org/2003/05/soap-encoding"
                         xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                        xmlns:v1="${url}"  
+                        xmlns:v1='${url}'  
                     >                    
                     <soapenv:Header>                         
                         <v1:clientAccountCode>${process.env.MEDIMPACT_CLIENT_CODE}</v1:clientAccountCode>
